d20ce8d2913af87a3c292798fbf5dc2cce7e7a80
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-20 18:42:11 -0600

    added random nmber generator string

--- api/random.py
+++ api/random.py
@@ -9,6 +9,7 @@ class handler(BaseHTTPRequestHandler):
         s = self.path
         url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
+        
         if query_string_list<0:
             random_number = 'not positive, think positive'
         random_number = random.randrange(0,query_string_list)
@@ -16,5 +17,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = f'Your random number is {random_number}'
+        message = str(f'Your random number is {random_number}')
         self.wfile.write(message.encode())
