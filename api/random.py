010df7de21a58e38f4bf6771e3b0cebd99b13d2c
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-20 18:45:18 -0600

    made change01 to random number generator string

--- api/random.py
+++ api/random.py
@@ -3,19 +3,18 @@ import random
 from urllib import parse
 
 
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
         url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
-        
-        if query_string_list<0:
-            random_number = 'not positive, think positive'
-        random_number = random.randrange(0,query_string_list)
+        query_string_list = int(query_string_list)
+        if query_string_list < 0:
+            random_number = "not positive, think positive"
+        random_number = random.randrange(0, query_string_list)
 
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
-        message = str(f'Your random number is {random_number}')
+        message = str(f"Your random number is {random_number}")
         self.wfile.write(message.encode())
